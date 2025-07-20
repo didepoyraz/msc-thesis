93a746a2045ce5b429cb276f38b8956f276a0757
didepoyraz <didepyz@gmail.com>
2025-07-20 18:39:33 +0100

    compared it with og tiling, og tiling is faster

--- vulkan/src/matmul/cpu_gpu_comb.c
+++ vulkan/src/matmul/cpu_gpu_comb.c
@@ -20,22 +20,6 @@
 #define offsetC(i, j, ldN) (i * ldN + j)
 #define NUM_THREADS 4
 
-void* cpu_worker(void* arg){
-    TileQueue* q = (TileQueue*) arg;
-    TileConfig tile;
-    DEBUG_PRINT("\nCPU threads starting up!");
-
-    while(dequeue_tile(q, &tile)) {
-        obj_t A_blis, B_blis, C_blis;
-        
-        bli_obj_create_with_attached_buffer(BLIS_FLOAT, q->matrix.BLOCK_SIZE, q->matrix.BLOCK_SIZE, q->matrix.A + offsetA(tile.i, tile.p, q->matrix.N), q->matrix.N, 1, &A_blis);
-        bli_obj_create_with_attached_buffer(BLIS_FLOAT, q->matrix.BLOCK_SIZE, q->matrix.BLOCK_SIZE, q->matrix.B + offsetB(tile.p, tile.j, q->matrix.N), q->matrix.N, 1, &B_blis);
-        bli_obj_create_with_attached_buffer(BLIS_FLOAT, q->matrix.BLOCK_SIZE, q->matrix.BLOCK_SIZE, q->matrix.C + offsetC(tile.i, tile.j, q->matrix.N), q->matrix.N, 1, &C_blis);
-        
-        bli_gemm(&BLIS_ONE, &A_blis, &B_blis, &BLIS_ONE, &C_blis);
-    }
-    return NULL;
-}
 
 int main(int argc, char* argv[]) {
 
@@ -71,25 +55,25 @@ int main(int argc, char* argv[]) {
         B[i] = (float)i+1;
     }
 
-    Mult matrix = {.A = A, .B = B, .C = C, .N = N, .BLOCK_SIZE = BLOCK_SIZE};
-    TileQueue queue = { .head = 0, .tail = 0, .size = 0, .done = false, .matrix = matrix};
+    // Mult matrix = {.A = A, .B = B, .C = C, .N = N, .BLOCK_SIZE = BLOCK_SIZE};
+    // TileQueue queue = { .head = 0, .tail = 0, .size = 0, .done = false, .matrix = matrix};
     
-    pthread_mutex_init(&queue.lock, NULL);
-    pthread_cond_init(&queue.not_empty, NULL);
+    // pthread_mutex_init(&queue.lock, NULL);
+    // pthread_cond_init(&queue.not_empty, NULL);
 
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     vulkan_init(A, B, C, N, BLOCK_SIZE);
-    bli_init(); 
+    // bli_init(); 
 
     DEBUG_PRINT("initialising threads!\n");
 
-    pthread_t threads[NUM_THREADS];
-    for(int i = 0; i < NUM_THREADS; i++) {
-        pthread_create(&threads[i], NULL, cpu_worker, &queue);
-    }
-    int cpu_gpu_ratio = 6;  
-    int count = 0;  
+    // pthread_t threads[NUM_THREADS];
+    // for(int i = 0; i < NUM_THREADS; i++) {
+    //     pthread_create(&threads[i], NULL, cpu_worker, &queue);
+    // }
+    // int cpu_gpu_ratio = 6;  
+    // int count = 0;  
 
     DEBUG_PRINT("\nstarting to submit tiles to the tile queue!\n");
 
@@ -99,29 +83,12 @@ int main(int argc, char* argv[]) {
                 TileConfig tile = {i, j, k};
                 // DEBUG_PRINT("\n\n-----submitting tile: (%d, %d, %d)-------\n", tile.i, tile.j, tile.p);
                 DEBUG_PRINT("\n======= submitting tile: A(%i, %i), B(%i, %i), C(%i, %i) =======\n", tile.i, tile.p, tile.p, tile.j, tile.i, tile.j);
-
-                if((count % (cpu_gpu_ratio + 1)) < cpu_gpu_ratio){
-                    //TODO: add error checking here
-                    enqueue_tile(&queue, tile);
-                }
-                else{
-                    DEBUG_PRINT("\nsubmitting to vulkan!\n");
-                    vulkan_submit_tile(tile.i, tile.p, tile.p, tile.j, tile.i, tile.j);
-                }
-                count++;
+                vulkan_submit_tile(tile.i, tile.p, tile.p, tile.j, tile.i, tile.j);
             }
         }
     }
 
-    pthread_mutex_lock(&queue.lock);
-    queue.done = true;
-    pthread_cond_broadcast(&queue.not_empty);
-    pthread_mutex_unlock(&queue.lock);
     // DEBUG_PRINT("BLIS default threads: %d\n", bli_thread_get_num_threads());
-
-    for (int i = 0; i < NUM_THREADS; i++){
-        pthread_join(threads[i], NULL);
-    }
 
     clock_gettime(CLOCK_MONOTONIC, &end);
     elapsed_full_execution = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
@@ -131,12 +98,11 @@ int main(int argc, char* argv[]) {
     printf("matrix c first element %f, last element %f", C[0], C[(N*N)-1]);
     // printf("BLIS GEMM Computation Time: %f ns\n----------------\n", elapsed);
     vulkan_print_total_time();
-
     // free everything
     free(A); free(B); free(C);
 
     vulkan_cleanup();
-    bli_finalize();
+    // bli_finalize();
 
     return 0;
 }

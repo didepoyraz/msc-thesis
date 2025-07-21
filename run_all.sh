59b152463cdfa4a1890147a6620b2e9f8f4954d2
didepoyraz <didepyz@gmail.com>
2025-07-21 14:08:05 +0100

    added a script

---
+++ run_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd vulkan/build/bin
+# List of (MATRIX_SIZE, BLOCK_SIZE) pairs
+pairs=(
+  "16 8"
+  "32 16"
+  "64 32"
+  "128 64"
+  "256 128"
+  "512 256"
+  "1024 512"
+  "2048 1024"
+  "4096 1024"
+)
+
+# Loop through each pair and call the program
+for pair in "${pairs[@]}"; do
+  read -r MATRIX_SIZE BLOCK_SIZE <<< "$pair"
+  echo "Running ./cpu_gpu_comb $MATRIX_SIZE $BLOCK_SIZE"
+  ./cpu_gpu_comb "$MATRIX_SIZE" "$BLOCK_SIZE"
+done

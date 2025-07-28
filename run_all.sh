ae4f72ffe2283e82efe061d7d6ec55280d8deaea
didepoyraz <didepyz@gmail.com>
2025-07-28 16:55:45 +0100

    added script to run all

--- run_all.sh
+++ run_all.sh
@@ -1,16 +1,21 @@
-cd src
-make clean
-make all MAT_SIZE=1024 TILE_SIZE=64
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./mat_mult
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./mat_mult_inter
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./mat_mult_tiles
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./neon
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./blis
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./strassen
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./neon_strassen
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./mpi_strassen
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./pthread_neon_strassen
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./para_blis
-perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,l2d_cache,l2d_cache_refill ./atlas
-make clean
-cd ..
+#!/bin/bash
+cd vulkan/build/bin
+# List of (MATRIX_SIZE, BLOCK_SIZE) pairs
+pairs=(
+  "16 8 4"
+  "32 16 8"
+  "64 16 8"
+  "128 16 8"
+  "256 16 8"
+  "512 16 8"
+  "1024 16 8"
+  "2048 16 8"
+  "4096 16 8"
+)
+
+# Loop through each pair and call the program
+for pair in "${pairs[@]}"; do
+  read -r MATRIX_SIZE BLOCK_SIZE RES_P_THREAD<<< "$pair"
+  echo "Running ./matmul $MATRIX_SIZE $BLOCK_SIZE $RES_P_THREAD"
+  ./matmul "$MATRIX_SIZE" "$BLOCK_SIZE"  "$RES_P_THREAD"
+done
--- vulkan/src/matmul/matmul.cpp
+++ vulkan/src/matmul/matmul.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <algorithm>
 #include <chrono>
+#include <iomanip> 
+#include <sstream>  
 
 #include <vulkan/vulkan.h>
 #include "VulkanTools.h"
@@ -567,6 +569,34 @@ public:
 
 			// Copy to output
 			memcpy(Output_Matrix.data(), mapped, bufferSize);
+			
+			std::string basePath = "/home/pi/Desktop/msc-thesis/vulkan/results/output_matrix";
+
+			std::ostringstream oss;
+			oss << basePath << "_" << N << "_" << TILE << ".csv";
+			std::string filename = oss.str();
+
+			std::ofstream outFile(filename);
+			if (!outFile.is_open()) {
+				std::cerr << "Error opening file for writing: " << filename << std::endl;
+				return;
+			}
+
+			outFile << std::fixed << std::setprecision(6);
+			for (int i = 0; i < N; ++i) {
+				for (int j = 0; j < N; ++j) {
+					outFile << Output_Matrix[i * N + j];
+					if (j < N - 1) {
+						outFile << ",";
+					}
+				}
+				outFile << "\n";
+			}
+
+			outFile.close();
+			std::cout << "Matrix saved to " << filename << std::endl;
+
+
 			vkUnmapMemory(device, hostMemoryC);
 		}
 
@@ -588,10 +618,11 @@ public:
 		// }
 
 		LOG("%f \t", Output_Matrix[0]);
-		LOG("First row of output matrix:\n");
-		for (int i = 0; i < N*N; ++i) {
-			LOG("%f \t", Output_Matrix[i]);
-		}
+		
+		// LOG("First row of output matrix:\n");
+		// for (int i = 0; i < N*N; ++i) {
+		// 	LOG("%f \t", Output_Matrix[i]);
+		// }
 
 		// std::cout << std::endl;
 
